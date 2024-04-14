2629266a3dfd2abf9157df6997ad3ac917425871
FearAndSelfLoathingInACardboardBox <treash.man.guy@gmail.com>
2024-04-13 22:29:50 -0700

    I should commit more often. >-> Modified engine.show to draw images Added engine.drawtext Added images Removed the placeholder text in myStory.json Altered css Moved title to appear after choices

--- engine.js
+++ engine.js
@@ -9,9 +9,23 @@ class Engine {
         this.firstSceneClass = firstSceneClass;
         this.storyDataUrl = storyDataUrl;
 
-        this.header = document.body.appendChild(document.createElement("h1"));
         this.output = document.body.appendChild(document.createElement("div"));
         this.actionsContainer = document.body.appendChild(document.createElement("div"));
+        this.header = document.body.appendChild(document.createElement("h1"));
+
+        document.body.style = 
+            `color: #fff;
+            background-color: #9f9db1;
+            font-family: 'Comic Sans MS','Chalkboard SE', 'Comic Neue',cursive;
+            text-shadow:
+                2px 2px 0 #72708c,
+                -2px 2px 0 #72708c,
+                -2px -2px 0 #72708c,
+                2px -2px 0 #72708c,
+                1px 1px 0 #72708c,
+                -1px 1px 0 #72708c,
+                -1px -1px 0 #72708c,
+                1px -1px 0 #72708c;`;
 
         fetch(storyDataUrl).then(
             (response) => response.json()
@@ -31,6 +45,20 @@ class Engine {
     addChoice(action, data) {
         let button = this.actionsContainer.appendChild(document.createElement("button"));
         button.innerText = action;
+        button.style = 
+            `color: #fff;
+            background-color: transparent;
+            border: 3px solid #72708c;
+            font-family: 'Comic Sans MS','Chalkboard SE', 'Comic Neue',cursive;
+            text-shadow:
+                2px 2px 0 #72708c,
+                -2px 2px 0 #72708c,
+                -2px -2px 0 #72708c,
+                2px -2px 0 #72708c,
+                1px 1px 0 #72708c,
+                -1px 1px 0 #72708c,
+                -1px -1px 0 #72708c,
+                1px -1px 0 #72708c;`;
         button.onclick = () => {
             while(this.actionsContainer.firstChild) {
                 this.actionsContainer.removeChild(this.actionsContainer.firstChild)
@@ -44,10 +72,83 @@ class Engine {
         this.header.innerText = title;
     }
 
-    show(msg) {
-        let div = document.createElement("div");
-        div.innerHTML = msg;
-        this.output.appendChild(div);
+    drawText(msg){
+        if (this.textbox)
+            while (this.textboxContent.length){
+                this.textbox.removeChild(this.textboxContent.pop())
+            }
+        else{
+            this.textbox = document.createElement("div");
+            this.textbox.style = 
+                `height:23%;
+                width: 65%;
+                position: relative;
+                top: 3.7%;
+                left: 2%;
+                font-size:3vmin;
+                text-shadow: none;`;
+            this.ui.appendChild(this.textbox);
+            this.textboxContent = []
+        }
+        let i = 1;   
+        let textcolor = "#fff";
+        let asteriskFlag = 1;
+        for (let x of msg){
+            this.textboxContent.unshift(document.createElement("span"));
+            this.textboxContent[0].style = ``;
+            this.textboxContent[0].style.color = textcolor;
+            if (x == ":" && msg[0] == "Y"){
+                textcolor = "#d0ffe2";
+            }else if (x == ":" && msg[0] == "C"){
+                textcolor = "#ffd5c9";
+            }else if (x == ":" && msg[0] == "D"){
+                textcolor = "#dac7ff";
+            }
+            this.textboxContent[0].textContent = x;
+            if (x == "," || x == "." || x == ":" || x == "-" || x == ";" || x == "!")
+                i += 2;
+            asteriskFlag ^= (x == "*");
+            i += asteriskFlag;
+            console.log(i);
+            this.textbox.appendChild(this.textboxContent[0]);
+        }
+    }
+
+    show(img) {
+        //modified to only update the div element rather then append new ones every passage
+        //also modified to display images rather than text
+        if (this.div)
+            this.div.style = 
+                `height: 68%;
+                width: 68%;
+                position: relative;
+                top: 1%;
+                left: 0.8%;
+                background:url("`+img+`");
+                background-repeat: no-repeat;
+                background-size:contain;`;
+        else{
+            this.ui = document.createElement("div");
+            this.div = document.createElement("div");
+            this.ui.style = 
+                `height: 100vh;
+                width: 129vh;
+                background:url("ui.png");
+                background-repeat: no-repeat;
+                background-size:contain;`;
+            this.div.style = 
+                `height: 68%;
+                width: 68%;
+                position: relative;
+                top: 1%;
+                left: 0.8%;
+                background:url("`+img+`");
+                background-repeat: no-repeat;
+                background-size:contain;`;
+            this.div.innerHTML = "";
+            this.output.appendChild(this.ui);
+            this.ui.appendChild(this.div);
+        }
     }
 }
 
--- rules.js
+++ rules.js
@@ -1,23 +1,106 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title);
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation);
+    }
+}
+
+class Inventory {
+    constructor(){
+        this.stuff = [];
+    }
+    addItem(item){
+        this.stuff.push(item);
+    }
+    hasItem(item){
+        for (let i = 0; i < this.stuff.length; i++){
+            if (this.stuff[i] == item)
+                return true;
+        }
+        return false;
+    }
+    removeItem(item){
+        temp = []
+        for (let i = 0; i < this.stuff.length; i++){
+            if (this.stuff[i] != item)
+                temp.push(item);
+        }
+        this.stuff = temp
+    }
+    removeNumOfItem(item,number){
+        temp = []
+        for (let i = 0; i < this.stuff.length; i++){
+            if ((this.stuff[i] != item) || (!number))
+                temp.push(item);
+            else
+                number --;
+        }
+        this.stuff = temp
+    }
+    describeItem(item){
+        switch(item){
+            case "Dryer Lint":
+                return "An allergy-inducing toxin produced in self-defense when humans attempt to bereave dryers of their clothing innards.";
+            case "Atom with Spin":
+                return "I could probably get a similar effect by flipping a coin blindfolded, but I'm broke. Also, I probably shouldn't have looked at these since observing them colapses the wave function.";
+            case "Atom":
+                return "100 picometers of everyones favorite flavors of quarks. Perfect for splitting with a friend!";
+            case "Chekov's Gun":
+                return "A loaded handgun. I'm not sure why it has the name 'Chekov' scrawled on it, or who that is, but it feels important.";
+            case "Cardboard Box":
+                return "Hehe B o x."; 
+            case "Sound Foam":
+                return "Foam that reduces sound. I put this up because my neighbores are just too loud! They'll scream stuff like 'HELP, I'VE BEEN TRAPPED HERE FOR TWELVE WEEKS,' and 'OH GOD- PLEASE, I JUST WANT TO SEE MY FAMILY AGAIN,' at all hours of the day. It's very rude!";
+            case "Fridge Magnets":
+                return "Magnets that at one point spelled out 'I <3 Death.' Now they spell '3ID e<t ha' which I think might be an anagram for 'I <3 Death.'";
+            case "Rat Poison":
+                return "Tasting notes: a little nutty; very bitter; a delicate sunscreen flavor towards the end; overall, I'd give it a 6/10.";
+            case "Superconductor":
+                return "Bitten by a radioactive piccoloist, Super Conductor effortlessly directs the simultaneous performance of an entire orchestra!"
+            case "Extension Cable":
+                return "My favorite fuseless, OSHA approved, eletric hazard. Seeing that this game cannonically takes place in Canada, this item doesn't make any sense but... uh... shut up!";
+            case "Water Tray":
+                return "Dihydrogen Monoxide constrained to a set volume! Oh, how I love playing god!";
+            case "Ice Tray":
+                return "A plastic tray with skull shaped patterns in each of the holes. I'd fill this with water but my water is broken. It's supposed to be a liquid but it comes out of the faucet as a supercritical fluid!";
+            case "Ice Ice Baby - Vanilla Ice":
+                return "The debut single by American rapper Vanilla Ice, K. Kennedy and DJ Earthquake. It's melting a little but it's still cold enough to get the job done.";
+            case "Bucket Cat":
+                return "Buckets hold liquids and cats are one of those! A match made in heaven!";
+            case "Cat Bucket":
+                return "Hmm, that doesn't look quite right...";
+            case "Cat & Bucket":
+                return "Man, I sure wish there was some kind of holding receptacle I could put this cat in so I wouldn't have to hold both it and this bucket... Wait, I have an idea!";
+            case "Lid":
+                return "This is a lid to an old tupperware container I had. I don't have the container part since there was a big bug on it once, and so I took the most reasonable course of action: I threw the whole thing out the window and then went outside and lit it on fire.";
+            case "Bucket":
+                return "Hehe B u c k e t.";
+            case "Oven Mitts":
+                return "A pair of oven mitts. I don't cook, though. I also don't own a pair of oven mitts. Where did these come from?";
+            case "Sardines":
+                return "Garbage-trash-fish in a cute little tin. I've been trying to get rid of these but they keep reappearing! I think they're mad at me...";
+            default:
+                return "Hmm, looks like this item is missing a description! Looks like somebody doesn't know how to program.";
+        }
+    }
+    drawItems(){
+
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
+        let locationData = this.engine.storyData.Locations[key]; 
+        this.engine.show(locationData.Image)
+        this.engine.drawText(locationData.Body);
         
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        if(locationData.Choices) {
+            for(let choice of locationData.Choices) { 
+                this.engine.addChoice(choice.Text,choice); 
             }
         } else {
             this.engine.addChoice("The end.")
@@ -26,7 +109,7 @@ class Location extends Scene {
 
     handleChoice(choice) {
         if(choice) {
-            this.engine.show("&gt; "+choice.Text);
+            //this.engine.show("&gt; "+choice.Text);
             this.engine.gotoScene(Location, choice.Target);
         } else {
             this.engine.gotoScene(End);
